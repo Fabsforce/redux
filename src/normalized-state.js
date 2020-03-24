19244aab68fc75fea8845e7cf860c0bcd070b238
Fabsforce <7438049+Fabsforce@users.noreply.github.com>
2020-03-24 19:13:13 +0100

    Modifications to make the app start

---
+++ src/normalized-state.js
@@ -0,0 +1,28 @@
+import {schema, normalize} from 'normalizr'
+import defaultState from './default-state'
+
+
+const user = new schema.Entity('users')
+const card = new schema.Entity('cards', {assignedTo: user})
+const list = new schema.Entity('lists', {
+        cards : [card]
+})
+
+// je te donne l'array d'objets lists et renvoie moi un array list selon le schema ci-dessus
+const normalizedLists = normalize(defaultState.lists, [list])
+const normalizedUsers = normalize(defaultState.users, [user])
+
+
+// Ma liste est normalisée j'en prends les entités lists
+export const lists = {
+    entities: normalizedLists.entities.lists,
+    ids: normalizedLists.result // l'array des ids après la normalization  (comme Steve nous a montré dans runkit => normalizr renvoie l'objetc modifié + un array d'ids des éléments modifiés)
+}
+
+// idem + haut
+export const cards = {
+    entities: normalizedUsers.entities.cards,
+    ids: Object.keys(normalizedLists.entities.cards)
+}
+
+export default {user, lists, cards}
---
+++ src/reducers/list-reducer.js
@@ -0,0 +1,9 @@
+import { lists as defaultLists } from '../normalized-state'
+
+
+const listsReducer = (lists = defaultLists, action) => {
+    console.log(lists, action)
+    return lists
+}
+
+export default listsReducer
